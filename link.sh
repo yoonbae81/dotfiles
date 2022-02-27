d55ca7974b88551af29eb3a5a18b72f4a21bd381
Yoonbae Cho <y@xcv.kr>
2022-02-27 20:25:08 +0400

    Update link.sh

--- link.sh
+++ link.sh
@@ -9,5 +9,6 @@ ln -sf $PWD/wgetrc      ~/.wgetrc
 
 mkdir -p ~/.config
 ln -sf $PWD/sway        ~/.config
+ln -sf $PWD/alacritty   ~/.config
 ln -sf $PWD/ranger      ~/.config
 ln -sf $PWD/fontconfig  ~/.config
